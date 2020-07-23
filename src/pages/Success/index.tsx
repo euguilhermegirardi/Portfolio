61f2bad364ca3ab11df5e04f66a278c603b397be
Guilherme Girardi <girardi.gui@icloud.com>
2020-07-23 15:02:44 +0100

    Create success page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import Nav from "../../components/Nav";
-import Footer from "../../components/Footer";
+import "../../scss/pages/Success/_success.scss";
 
 export default function Success() {
    return (
       <>
-         <Nav />
          <section className="success">
             <div className="success-container">
-               <h2>Thanks!</h2>
+               <h2 className="success__h2">
+                  Thank you very much for sending me this message!
+               </h2>
+               <p className="success__p">
+                  I will get back to you as soon as possible! Have a nice day!
+               </p>
+
+               <button className="btn">
+                  <Link to="/">Back to Home</Link>
+               </button>
             </div>
          </section>
-         <Footer />
       </>
    );
 }

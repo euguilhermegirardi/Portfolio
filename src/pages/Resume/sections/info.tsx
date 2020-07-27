541f610dc9d9e8d8d94e21c0fa59d7ddc3cf33db
Guilherme Girardi <girardi.gui@icloud.com>
2020-07-27 19:17:50 +0100

    Edit scss for responsive

--- src/pages/Resume/sections/info.tsx
+++ src/pages/Resume/sections/info.tsx
@@ -12,81 +12,83 @@ import profile from "../../../assets/Home/profile.jpeg";
 
 export default function Info() {
    return (
-      <section className="info-resume">
-         <div className="info-resume__container">
-            <img
-               className="info__img--resume"
-               src={profile}
-               alt="profile.jpeg"
-            />
-            <div className="info__container info__container--resume">
-               <div className="info__profile-container">
-                  <h2 className="h2 info__h2">Guilherme Girardi</h2>
-                  <h3 className="info__h3">Frontend Developer</h3>
-                  <div className="info__small-container">
-                     <div className="info__icon-container">
-                        <AiOutlineMail className="info__icon" />
+      <div className="container">
+         <section className="info-resume">
+            <div className="info-resume__container">
+               <img
+                  className="info__img--resume"
+                  src={profile}
+                  alt="profile.jpeg"
+               />
+               <div className="info__container info__container--resume">
+                  <div className="info__profile-container">
+                     <h2 className="h2 info__h2">Guilherme Girardi</h2>
+                     <h3 className="info__h3">Frontend Developer</h3>
+                     <div className="info__small-container">
+                        <div className="info__icon-container">
+                           <AiOutlineMail className="info__icon" />
+                        </div>
+                        <small className="info__small">
+                           girardi.gui@icloud.com
+                        </small>
                      </div>
-                     <small className="info__small">
-                        girardi.gui@icloud.com
-                     </small>
-                  </div>
-                  <div className="info__small-container">
-                     <div className="info__icon-container">
-                        <FaMobileAlt className="info__icon" />
+                     <div className="info__small-container">
+                        <div className="info__icon-container">
+                           <FaMobileAlt className="info__icon" />
+                        </div>
+                        <small className="info__small">+353 083 380 0087</small>
                      </div>
-                     <small className="info__small">+353 083 380 0087</small>
                   </div>
-               </div>
-               <div className="info__media-container">
-                  <div className="info__small-container">
-                     <a
-                        className="info__anchor"
-                        href="https://www.linkedin.com/in/guilherme-girardi-1aa6a1185/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                     >
-                        <div className="info__icon-container">
-                           <FaLinkedin className="info__icon" />
-                        </div>
-                        <small className="info__small">
-                           linkedin.com/in/guilherme-girardi-1aa6a1185/
-                        </small>
-                     </a>
-                  </div>
-                  <div className="info__small-container">
-                     <a
-                        className="info__anchor"
-                        href="https://github.com/euguilhermegirardi"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                     >
-                        <div className="info__icon-container">
-                           <FaGithubSquare className="info__icon" />
-                        </div>
-                        <small className="info__small">
-                           github.com/euguilhermegirardi
-                        </small>
-                     </a>
-                  </div>
-                  <div className="info__small-container">
-                     <a
-                        className="info__anchor"
-                        href="https://www.instagram.com/euguilhermegirardi/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                     >
-                        <div className="info__icon-container">
-                           <FaInstagramSquare className="info__icon" />
-                        </div>
-                        <small className="info__small">
-                           instagram.com/euguilhermegirardi/
-                        </small>
-                     </a>
+                  <div className="info__media-container">
+                     <div className="info__small-container info__small-container--right">
+                        <a
+                           className="info__anchor"
+                           href="https://www.linkedin.com/in/guilherme-girardi-1aa6a1185/"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                        >
+                           <div className="info__icon-container">
+                              <FaLinkedin className="info__icon" />
+                           </div>
+                           <small className="info__small">
+                              linkedin.com/in/guilherme-girardi-1aa6a1185/
+                           </small>
+                        </a>
+                     </div>
+                     <div className="info__small-container info__small-container--right">
+                        <a
+                           className="info__anchor"
+                           href="https://github.com/euguilhermegirardi"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                        >
+                           <div className="info__icon-container">
+                              <FaGithubSquare className="info__icon" />
+                           </div>
+                           <small className="info__small">
+                              github.com/euguilhermegirardi
+                           </small>
+                        </a>
+                     </div>
+                     <div className="info__small-container info__small-container--right">
+                        <a
+                           className="info__anchor"
+                           href="https://www.instagram.com/euguilhermegirardi/"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                        >
+                           <div className="info__icon-container">
+                              <FaInstagramSquare className="info__icon" />
+                           </div>
+                           <small className="info__small">
+                              instagram.com/euguilhermegirardi/
+                           </small>
+                        </a>
+                     </div>
                   </div>
                </div>
             </div>
-         </div>
-      </section>
+         </section>
+      </div>
    );
 }
